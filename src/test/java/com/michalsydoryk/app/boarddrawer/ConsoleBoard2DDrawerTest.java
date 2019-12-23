795c275c198393f9e1877b7e13c1075df5705c92
Michal Sydoryk <michal_sydoryk@epam.com>
2019-12-23 10:03:34 +0100

    Add test to BoardPrinter

--- src/test/java/com/michalsydoryk/app/boarddrawer/ConsoleBoard2DDrawerTest.java
+++ src/test/java/com/michalsydoryk/app/boarddrawer/ConsoleBoard2DDrawerTest.java
@@ -6,6 +6,7 @@ import com.michalsydoryk.app.board.Board2D;
 import com.michalsydoryk.app.coordinates.Coordinates2D;
 import com.michalsydoryk.app.coordinatescomparator.Coordinates2DComparator;
 import com.michalsydoryk.app.sign.Sign;
+import org.testng.Assert;
 import org.testng.ITestNGListener;
 import org.testng.annotations.Test;
 
@@ -33,22 +34,38 @@ public class ConsoleBoard2DDrawerTest {
         board.addField(new Coordinates2D(24, 24), Sign.NAUGHT);
     }
 
-    private String drawIndexesLine(){
-        int boardSize = board.getSize();
-        int numberOfLines = String.valueOf(boardSize).length();
-        int numberOfLastSigns = boardSize%10;
-        String empty10 = "          ";
-        String form0To9 = "0 1 2 3 4 5 6 7 8 9 ";
-        for(int i = 0; i<numberOfLines; i++){
+    private void prepare10X10Board(Board board){
+        board.addField(new Coordinates2D(0, 0), Sign.CROSS);
+        board.addField(new Coordinates2D(1, 3), Sign.CROSS);
+        board.addField(new Coordinates2D(9, 4), Sign.CROSS);
 
-        }
-        return " ";
     }
 
-    public void checkRow(){
-        ConsoleBoard2DDrawer boardDrawer = new ConsoleBoard2DDrawer(board);
-        prepareBoard();
-        System.out.println(boardDrawer.draw());
+
+    public void shouldPrintSameString(){
+        //given
+        int boardSize = 10;
+        int combinationSize = 5;
+        Board board10x10 = new Board2D(boardSize, combinationSize);
+        ConsoleBoard2DDrawer boardDrawer = new ConsoleBoard2DDrawer(board10x10);
+        prepare10X10Board(board10x10);
+        //when
+        String toDraw = boardDrawer.draw();
+        String expectedResult = "                       \n" +
+                                "   0 1 2 3 4 5 6 7 8 9 \n" +
+                                " 0 X|_|_|_|_|_|_|_|_|_\n" +
+                                " 1 _|_|_|_|_|_|_|_|_|_\n" +
+                                " 2 _|_|_|_|_|_|_|_|_|_\n" +
+                                " 3 _|X|_|_|_|_|_|_|_|_\n" +
+                                " 4 _|_|_|_|_|_|_|_|_|X\n" +
+                                " 5 _|_|_|_|_|_|_|_|_|_\n" +
+                                " 6 _|_|_|_|_|_|_|_|_|_\n" +
+                                " 7 _|_|_|_|_|_|_|_|_|_\n" +
+                                " 8 _|_|_|_|_|_|_|_|_|_\n" +
+                                " 9  | | | | | | | | | \n";
+        //then
+        System.out.println(toDraw);
+        Assert.assertEquals(toDraw, expectedResult);
 
     }
 
